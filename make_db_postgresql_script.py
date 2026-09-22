a136dded5131a669a24c6c348369c593e25f179d
Kirill Startsev <166030822+Kirill-pydev@users.noreply.github.com>
2026-09-22 23:50:31 +0400

    Update and rename make_db_postgresql.py to make_db_postgresql_script.py

--- make_db_postgresql_script.py
+++ make_db_postgresql_script.py
@@ -1,10 +1,14 @@
 import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
 conn = psycopg2.connect(
-    dbname='НАЗВАНИЕ БАЗЫ ДАННЫХ',
-    user='postgres',
-    password='ПАРОЛЬ',
-    host='localhost',
-    port='5432'
+    dbname=os.getenv("DB_NAME", "postgres"),
+    user=os.getenv("DB_USER", "postgres"),
+    password=os.getenv("DB_PASSWORD", "postgres"),
+    host=os.getenv("DB_HOST", "localhost"),
+    port=os.getenv("DB_PORT", "5432")
 )
 
 cursor = conn.cursor()
